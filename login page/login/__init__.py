37ed072a1c7956091dcc26c6bf5990075358061e
SohaKhare <sohaajaykhare2006@gmail.com>
2024-08-25 03:46:39 +0530

    Changes to be committed: 	modified:   login page/login/__init__.py 	new file:   login page/login/__pycache__/__init__.cpython-311.pyc

--- login page/login/__init__.py
+++ login page/login/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///login.db'
 app.config['SECRET_KEY'] = '2ab28c0f6afeb78a44be0807'
 db = SQLAlchemy(app)
 app.app_context().push()
